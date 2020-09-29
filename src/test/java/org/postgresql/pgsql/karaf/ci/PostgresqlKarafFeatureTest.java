711f7f363a9bc4253c7ad7c91e34424d74d5f317
Steinar Bang <sb@dod.no>
2020-09-29 15:50:29 +0200

    Use a the karaf integration test framwork as basis for the integration test

--- src/test/java/org/postgresql/pgsql/karaf/ci/PostgresqlKarafFeatureTest.java
+++ src/test/java/org/postgresql/pgsql/karaf/ci/PostgresqlKarafFeatureTest.java
@@ -1,16 +1,35 @@
 package org.postgresql.pgsql.karaf.ci;
 
+import java.util.stream.Stream;
+
+import org.apache.karaf.itests.KarafTestSupport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
+import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-class PostgresqlKarafFeatureTest {
+public class PostgresqlKarafFeatureTest extends KarafTestSupport {
+
+    @Configuration
+    public Option[] config() {
+        MavenArtifactUrlReference postgresqlFeatureRepo = maven().groupId("org.postgresql").artifactId("postgresql").versionAsInProject().type("xml").classifier("features");
+        Option[] options = new Option[] {
+                features(postgresqlFeatureRepo)
+        };
+        return Stream.of(super.config(), options).flatMap(Stream::of).toArray(Option[]::new);
+    }
 
     @Test
-    void testLoadFeature() throws Exception {
+    public void testLoadFeature() throws Exception {
+        installAndAssertFeature("postgresql");
     }
 
 }
